d5e4be6fa9b30383ea23090f08a1b3e985ad6243
Josue Rivas <reycoc2311@gmail.com>
2022-07-04 12:04:24 -0600

    ISR: implemented for pokemons by name & id

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -138,7 +138,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map((name) => ({
       params: { name },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
@@ -147,10 +147,22 @@ export const getStaticProps: GetStaticProps = async ({
 }) => {
   const { name } = params as { name: string };
 
+  const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name),
+      pokemon,
     },
+    revalidate: 86400,
   };
 };
 
